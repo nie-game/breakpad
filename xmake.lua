3859f7827792cb2a9f6fcd998ad1ceac135bc7b1
Christian Kurz <chrikuvellberg@gmail.com>
2025-11-03 00:45:07 +0100

    attempt custom dia sdk finder

--- xmake.lua
+++ xmake.lua
@@ -25,7 +25,7 @@ do
 
       local dia_sdk = find_dia_sdk(nil, {arch = target:arch()})
       assert(dia_sdk)
-      add({includedirs = dia_sdk.includedirs, linkdirs = dia_sdk.linkdirs, links = {"diaguids"}})
+      target;add({includedirs = dia_sdk.includedirs, linkdirs = dia_sdk.linkdirs, links = {"diaguids"}})
     end)
     add_files("src/processor/*.cc")
     remove_files("src/processor/*test*.cc", "src/processor/microdump_stackwalk.cc", "src/processor/synth_minidump.cc",
